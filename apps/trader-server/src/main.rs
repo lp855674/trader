2a447436c845bdee311967c24a142da096df6ed9
liuping <453923278@qq.com>
2026-06-01 10:45:01 +0800

    chore: polish workspace scaffold

--- apps/trader-server/src/main.rs
+++ apps/trader-server/src/main.rs
@@ -3,6 +3,6 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    tracing::info!("trader-server starting");
+    tracing::info!(channel = "server", "server starting");
     Ok(())
 }
